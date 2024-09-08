fa600b6eceecd0b8d9c52f4fe8dded5a8d31e9d1
waivan <31401897+waivan@users.noreply.github.com>
2024-09-08 13:19:18 +0200

    Product Crud complete

--- Backend/Controllers/productController.js
+++ Backend/Controllers/productController.js
@@ -1,1 +1,10 @@
-const router = require("express").Router()
+const router = require("express").Router();
+const productModel = require("../Models/productModel");
+
+router.post("/", productModel.addProduct);
+router.get("/", productModel.getAllProducts);
+router.get("/:id", productModel.getOneProduct);
+router.put("/:id", productModel.updateProductPrice);
+router.delete("/:id", productModel.removeProduct);
+
+module.exports = router;
